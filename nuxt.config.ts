77544e98104ff58d4e56d9485a6f05f8a702c6a9
Gustavo Cabral <gustavocabral@cbpf.br>
2025-12-22 19:34:12 -0300

    mudança no nuxtconfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import process from 'node:process'
 
-const { PRODUCTION, SITE_URL, NUXT_SESSION_PASSWORD, MAIL_PASSWORD, MAIL_URL, MAIL_PORT, MAIL_SSL, MAIL_USERNAME, DEV_URL, DEV_KEY, DEV_CERT, MP_ACCESS_TOKEN, GOOGLE_SHEETS_WEBHOOK_URL } = process.env
+const { PRODUCTION, SITE_URL, MAIL_PASSWORD, MAIL_URL, MAIL_PORT, MAIL_SSL, MAIL_USERNAME, DEV_URL, DEV_KEY, DEV_CERT } = process.env
 
 export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxt/image', '@nuxtjs/seo', '@vueuse/nuxt'],
@@ -18,14 +18,10 @@ export default defineNuxtConfig({
     email: { url: MAIL_URL, port: MAIL_PORT, ssl: MAIL_SSL, username: MAIL_USERNAME, password: MAIL_PASSWORD },
     production: PRODUCTION,
     url: SITE_URL,
-    sessionPassword: NUXT_SESSION_PASSWORD,
-    mpAccessToken: MP_ACCESS_TOKEN,
-    googleSheetsUrl: GOOGLE_SHEETS_WEBHOOK_URL,
   },
   devServer: {
-
-    // host: DEV_URL,
-    // https: DEV_KEY && DEV_CERT ? { key: DEV_KEY, cert: DEV_CERT } : undefined,
+    host: DEV_URL,
+    https: DEV_KEY && DEV_CERT ? { key: DEV_KEY, cert: DEV_CERT } : undefined,
   },
   compatibilityDate: '2025-08-01',
   nitro: { experimental: { asyncContext: true } },
